353c4df7d4c127b6429c9b01fbc9619a36557801
Brian Battauz <122492710+Brian13b@users.noreply.github.com>
2025-08-13 22:17:08 -0300

    Cambios en la direccion del logo

--- quico_basquet_frontend/src/components/Header.jsx
+++ quico_basquet_frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
     <header>
       <div className="container">
         <div className="logo">
-          <img src="quico_basquet_frontend\public\assets\logo.png" alt="Quico Basquet Logo" />
+          <img src="\assets\logo.png" alt="Quico Basquet Logo" />
         </div>
 
         <div className="login">
